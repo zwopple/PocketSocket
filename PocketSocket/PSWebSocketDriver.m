f6f6b3464791ebc8f83b12999369c3edc07eaf8e
Robert Payne <robertpayne@me.com>
2016-03-30 14:47:35 +1300

    Project fixes and updated copyrights

--- PocketSocket/PSWebSocketDriver.m
+++ PocketSocket/PSWebSocketDriver.m
@@ -1,4 +1,4 @@
-//  Copyright 2014 Zwopple Limited
+//  Copyright 2014-Present Zwopple Limited
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -83,9 +83,9 @@ typedef NS_ENUM(NSInteger, PSWebSocketDriverState) {
 + (BOOL)isWebSocketRequest:(NSURLRequest *)request {
     NSDictionary *headers = request.allHTTPHeaderFields;
     
-    NSSet *version = PSHTTPHeaderFieldValues([headers[@"Sec-WebSocket-Version"] lowercaseString]);
-    NSSet *upgrade = PSHTTPHeaderFieldValues([headers[@"Upgrade"] lowercaseString]);
-    NSSet *connection = PSHTTPHeaderFieldValues([headers[@"Connection"] lowercaseString]);
+    NSOrderedSet *version = PSHTTPHeaderFieldValues([headers[@"Sec-WebSocket-Version"] lowercaseString]);
+    NSOrderedSet *upgrade = PSHTTPHeaderFieldValues([headers[@"Upgrade"] lowercaseString]);
+    NSOrderedSet *connection = PSHTTPHeaderFieldValues([headers[@"Connection"] lowercaseString]);
     
     if(headers[@"Sec-WebSocket-Key"] &&
        [version containsObject:@"13"] &&
--- PocketSocket/PSWebSocketUTF8Decoder.m
+++ PocketSocket/PSWebSocketUTF8Decoder.m
@@ -1,4 +1,4 @@
-//  Copyright 2014 Zwopple Limited
+//  Copyright 2014-Present Zwopple Limited
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
