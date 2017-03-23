48fb7036f9591c03a7020bf29e84ae0af8678772
Ngoc Truong <ngocbkhn@gmail.com>
2017-03-24 10:10:18 +1100

    Allow to start the server at a dynamic available port Fix issues with block reference Code formatting followed Clang style

--- PSAutobahnServerTests/AppDelegate.m
+++ PSAutobahnServerTests/AppDelegate.m
@@ -11,45 +11,54 @@
 
 @interface AppDelegate () <PSWebSocketServerDelegate>
 
-@property (nonatomic, strong) PSWebSocketServer *server;
+@property(nonatomic, strong) PSWebSocketServer *server;
 
 @end
 
 @implementation AppDelegate
 
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  // Override point for customization after application launch.
+  // Create a local server at a random available port
+  self.server = [PSWebSocketServer localServer];
+  self.server.delegate = self;
+  [self.server start];
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
-    
-    self.server = [PSWebSocketServer serverWithHost:@"127.0.0.1" port:9001];
-    self.server.delegate = self;
-    [self.server start];
-    
-    return YES;
+  return YES;
 }
 
 #pragma mark - PSWebSocketServerDelegate
 
 - (void)serverDidStart:(PSWebSocketServer *)server {
+  NSLog(@"WebSockets Server started at port %zd", server.port);
 }
 - (void)server:(PSWebSocketServer *)server didFailWithError:(NSError *)error {
-    [NSException raise:NSInternalInconsistencyException format:error.localizedDescription];
+  [NSException raise:NSInternalInconsistencyException
+              format:@"didFailWithError: %@", error.localizedDescription];
 }
 - (void)serverDidStop:(PSWebSocketServer *)server {
-    [NSException raise:NSInternalInconsistencyException format:@"Server stopped unexpected."];
+  [NSException raise:NSInternalInconsistencyException
+              format:@"Server stopped unexpected."];
 }
 
-- (void)server:(PSWebSocketServer *)server webSocketDidOpen:(PSWebSocket *)webSocket {
-    
+- (void)server:(PSWebSocketServer *)server
+    webSocketDidOpen:(PSWebSocket *)webSocket {
 }
-- (void)server:(PSWebSocketServer *)server webSocket:(PSWebSocket *)webSocket didReceiveMessage:(id)message {
-    [webSocket send:message];
+- (void)server:(PSWebSocketServer *)server
+            webSocket:(PSWebSocket *)webSocket
+    didReceiveMessage:(id)message {
+  [webSocket send:message];
 }
-- (void)server:(PSWebSocketServer *)server webSocket:(PSWebSocket *)webSocket didFailWithError:(NSError *)error {
-    
+- (void)server:(PSWebSocketServer *)server
+           webSocket:(PSWebSocket *)webSocket
+    didFailWithError:(NSError *)error {
 }
-- (void)server:(PSWebSocketServer *)server webSocket:(PSWebSocket *)webSocket didCloseWithCode:(NSInteger)code reason:(NSString *)reason wasClean:(BOOL)wasClean {
-    
+- (void)server:(PSWebSocketServer *)server
+           webSocket:(PSWebSocket *)webSocket
+    didCloseWithCode:(NSInteger)code
+              reason:(NSString *)reason
+            wasClean:(BOOL)wasClean {
 }
 
 @end
