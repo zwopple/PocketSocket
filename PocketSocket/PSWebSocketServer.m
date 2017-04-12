5c46ddde3a2aa3c6df26d4bea4784b7f902ccc33
CHEN Xian-an <xianan.chen@gmail.com>
2017-04-13 02:48:30 +0800

    Use kCFRunLoopCommonModes

--- PocketSocket/PSWebSocketServer.m
+++ PocketSocket/PSWebSocketServer.m
@@ -186,7 +186,7 @@ void PSWebSocketServerAcceptCallback(CFSocketRef s, CFSocketCallBackType type, C
     _socketRunLoopSource = CFSocketCreateRunLoopSource(kCFAllocatorDefault, _socket, 0);
     
     CFRunLoopRef runLoop = [[self runLoop] getCFRunLoop];
-    CFRunLoopAddSource(runLoop, _socketRunLoopSource, kCFRunLoopDefaultMode);
+    CFRunLoopAddSource(runLoop, _socketRunLoopSource, kCFRunLoopCommonModes);
     
     _running = YES;
     
@@ -218,7 +218,7 @@ void PSWebSocketServerAcceptCallback(CFSocketRef s, CFSocketCallBackType type, C
 - (void)disconnect:(BOOL)silent {
     if(_socketRunLoopSource) {
         CFRunLoopRef runLoop = [[self runLoop] getCFRunLoop];
-        CFRunLoopRemoveSource(runLoop, _socketRunLoopSource, kCFRunLoopDefaultMode);
+        CFRunLoopRemoveSource(runLoop, _socketRunLoopSource, kCFRunLoopCommonModes);
         CFRelease(_socketRunLoopSource);
         _socketRunLoopSource = nil;
     }
